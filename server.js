2fd07b036a913e6a3e8bb90c61797bc4ffa8417b
viditjain2508 <rsvr516@gmail.com>
2024-01-02 16:47:14 +0530

    Updated again

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ dotenv.config();
 
 const connection = mysql.createConnection({
     host: process.env.DATABASE_HOST,
-    user: process.env.DATABSE_USER,
-    password: process.env.DATABSE_PASSWORD,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
 });
 
 connection.connect((err) => {
